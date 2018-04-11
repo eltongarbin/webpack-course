9db673894abef71bb0e24cde44cc4dfbb835c561
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-10 23:25:00 -0300

    React Universal Components

--- config/webpack.prod-server.js
+++ config/webpack.prod-server.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var nodeExternals = require('webpack-node-externals');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const externals = require('./node-externals');
 
 module.exports = {
   name: 'server',
   target: 'node',
-  externals: nodeExternals(),
+  externals,
   entry: './src/server/render.js',
   mode: 'development',
   output: {
     filename: 'prod-server-bundle.js',
+    chunkFilename: '[name].js',
     path: path.resolve(__dirname, '../build'),
     libraryTarget: 'commonjs2'
   },
@@ -23,39 +25,20 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { minimize: true, sourceMap: false }
-            },
-            {
-              loader: 'postcss-loader',
-              options: { sourceMap: false }
-            }
-          ]
-        })
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' }
+        ]
       },
       {
         test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { minimize: true, sourceMap: false }
-            },
-            {
-              loader: 'postcss-loader',
-              options: { sourceMap: false }
-            },
-            {
-              loader: 'less-loader',
-              options: { sourceMap: false }
-            }
-          ]
-        })
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'less-loader' }
+        ]
       },
       {
         test: /\.jpg$/,
@@ -80,7 +63,10 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('[name].css'),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
+    // new ExtractTextPlugin('[name].css'),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
